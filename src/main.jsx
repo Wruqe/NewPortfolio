9bcbc96f3227b0b0223149ddb9eec1d570751c1b
Ernest Jones <ernest.jones30@yahoo.com>
2024-12-23 19:55:05 -0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import emailjs from "@emailjs/browser";
-
-emailjs.init("YOUR_PUBLIC_KEY"); // Replace with your actual public key
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
